c08b5ea68c3e4fc0e02a2a9883f801ef09b3ccf3
Vladislav <imfoslash@gmail.com>
2024-06-19 21:20:34 +0300

    добавил сбор данный по игрокам и событию

---
+++ ao_event_parser.py
@@ -0,0 +1,60 @@
+import requests
+import json
+
+class Event_parser:
+    @staticmethod
+    def get_event_data(event_id: int):
+        url = f"https://gameinfo.albiononline.com/api/gameinfo/events/{event_id}"
+
+        event_data = requests.get(url)
+
+        return json.loads(event_data.content)
+        # return json.load(event_data.text)
+
+    @staticmethod
+    def parse_equip(equip: dict):
+        res = {}
+        for cell in equip:
+            res[cell] = equip[cell]['Type'] if equip[cell] is not None else None
+            # item_type = item_cell['Type']
+            # enchantment = item_type.split('@')[1] if '@' in item_type else 0
+            # quality = item_cell['Quality'] if item_cell['Quality'] is not None else None
+            # res[item_cell] = item_type
+        
+        return res
+    
+    @staticmethod
+    def parse_event_info(data):
+        pricipant_data = {
+            'groupMemberCount': data['groupMemberCount'],
+            'event_id': data['EventId'],
+            'time_stamp': data['TimeStamp'],
+            'gvg_match': data['GvGMatch'],
+            'kill_area': data['KillArea'],
+        }
+
+        return pricipant_data
+
+
+
+            
+
+    @staticmethod
+    def parse_player_data(player_data: dict, pricipant_type):
+        if pricipant_type == 'killer':
+            pricipant = player_data['Killer']
+        elif pricipant_type == 'victim':
+            pricipant = player_data['Victim']
+
+        pricipant_data = {
+            'average_itemPower': pricipant['AverageItemPower'],
+            # 'equip': pricipant['Equipment'],
+            'equip': Event_parser.parse_equip(pricipant['Equipment']),
+            'name': pricipant['Name'],
+            'guild_name': pricipant['GuildName'],
+            'alliance_name': pricipant['AllianceName'],
+            'kill_fame': pricipant['KillFame'],
+        }
+
+        return pricipant_data
+    
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from ao_parser import Parser
+from ao_event_parser import Event_parser
+import pprint
 
 def main():
     event_id = 1051418761
 
-    data = Parser.get_event_data(event_id)
-    print(data)
+    event_data = Event_parser.get_event_data(event_id)
+    event_info = Event_parser.parse_event_info(event_data)
+    victim_info = Event_parser.parse_player_data(event_data, 'victim')
+    killer_info = Event_parser.parse_player_data(event_data, 'killer')
+
+    pprint.pp(event_info)
+    pprint.pp(victim_info)
+    pprint.pp(killer_info)
 
 
 
